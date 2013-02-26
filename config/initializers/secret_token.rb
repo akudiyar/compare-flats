96114cd3df4e96d192e60e036ebcc39ad4781067
akudiyar <akudiyar@gmail.com>
2013-02-26 23:45:30 +0400

    Initial commit (no real work)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flats::Application.config.secret_token = '6f898cd6bd0b8a439055c941ce0989661fdd8f13e6d7eb2ca94523640557cdb7bf7bacfa36bbb13772df3bec9018da9b7d7c4ed5ac093184871afe9fee952713'
